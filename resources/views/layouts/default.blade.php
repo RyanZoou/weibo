ad02aaefd91c494a43350a11373c897187a711d7
RyanZou <ryanzoou@163.com>
2019-03-16 20:58:39 +0800

    用户显示界面

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title>@yield('title', 'weibo app')-高大上微博</title>
-  <link rel="stylesheet" type="text/css" href=".{{mix('css/app.css')}}">
+  <link rel="stylesheet" type="text/css" href="http://localhost/weibo/public/css/app.css">
 </head>
 <body>
   @include('layouts._header')
