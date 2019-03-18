8a5b5d908e9ba56dd359acb3335ffee9432ff547
RyanZou <ryanzoou@163.com>
2019-03-18 13:35:54 +0800

    更改用户资料

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,12 +10,12 @@ class UserController extends Controller
 {
     public function create()
     {
-        return view('user.create');
+        return view('users.create');
     }
 
     public function show(User $user)
     {
-        return view('user.show', compact('user'));
+        return view('users.show', compact('user'));
     }
 
     public function store(Request $request)
@@ -35,5 +35,10 @@ class UserController extends Controller
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
     }
 }
