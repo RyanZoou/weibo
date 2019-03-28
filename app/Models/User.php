cd38b7f599f8e9384e64be97579ca679d996c195
RyanZou <ryanzoou@163.com>
2019-03-28 23:51:47 +0800

    用户微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use App\Models\Status;
 
 class User extends Authenticatable
 {
@@ -61,7 +60,7 @@ class User extends Authenticatable
 
     public function statuses()
     {
-        return $this->hasMany(Status:class);
+        return $this->hasMany(Status::class);
     }
 
 }
